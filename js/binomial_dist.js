0013372366bd5d9ee1b508325ebf42094f84d853
sdawidowich <79337885+sdawidowich@users.noreply.github.com>
2023-10-10 15:29:50 -0500

    Add binomial distribution caclulation file and functions.

---
+++ js/binomial_dist.js
@@ -0,0 +1,38 @@
+function calc_expected_value(n, p) {
+    return n * p;
+}
+
+function calc_variance(n, p) {
+    return n * p * (1 - p);
+}
+
+function calc_prob(n, p, x) {
+    return calc_combinations(n, x) * Math.pow(p, x) * Math.pow(1 - p, n - x);
+}
+
+function calc_dist() {
+    let n = parseInt(document.getElementById("n-trials").value);
+    let p = parseFloat(document.getElementById("p-success").value);
+
+    if (isNaN(n) || isNaN(p)) {
+        return;
+    }
+
+    let binomial_dist = {
+        expected_value: calc_expected_value(n, p),
+        variance: calc_variance(n, p),
+        probabilities: [],
+        cumProb: []
+    }
+
+    for (let i = 0; i <= n; i++) {
+        binomial_dist.probabilities.push(calc_prob(n, p, i));
+    }
+
+    binomial_dist.cumProb.push(binomial_dist.probabilities[0]);
+    for (let i = 1; i <= n; i++) {
+        binomial_dist.cumProb.push(binomial_dist.cumProb[i - 1] + binomial_dist.probabilities[i]);
+    }
+
+    return binomial_dist;
+}
