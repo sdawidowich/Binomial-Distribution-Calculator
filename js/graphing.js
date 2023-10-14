4e2b9f6e17ec11d40cc45ca18207139caef8ae32
sdawidowich <79337885+sdawidowich@users.noreply.github.com>
2023-10-14 17:44:17 -0500

    Add tooltip to graph to show the probability when hovering over the rectangles.

--- js/graphing.js
+++ js/graphing.js
@@ -1,6 +1,7 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 export function graph_dist(dist) {
+    // Filter out useless data (Probabilities that are practically 0)
     const keys = Object.keys(dist.probabilities);
     const data = keys.filter((key) => {
         return dist.probabilities[key] > 0.0000001;
@@ -27,7 +28,7 @@ export function graph_dist(dist) {
         .domain([0, d3.max(data, (d) => dist.probabilities[d] + 0.025)])
         .range([height - margin.bottom, margin.top]);
 
-
+    // Remove existing graph if it exists
     d3.select(".dist-output .dist-graph svg").remove("svg");
 
     // Create the SVG container.
@@ -35,16 +36,35 @@ export function graph_dist(dist) {
     svg.attr("width", width)
         .attr("height", height)
         .attr("viewBox", [0, 0, width, height]);
-    
-    // Create Rect
+
+    // Create tooltip when user hovers over a bar
+    const tooltip = d3.select('#tooltip');
+    const mouseover = (event, d) => {
+        tooltip.style("opacity", 0.9);
+    };
+    const mouseleave = (event, d) => {
+        tooltip.style('opacity', 0);
+    }
+    const mousemove = (event, d) => {
+        const x = event.pageX + 15, y = event.pageY - 60;
+        tooltip.select("label").text(`P(X = ${d})`);
+        tooltip.select("#tooltip-val").text(`= ${dist.probabilities[d].toFixed(5)}`);
+        tooltip.style('left', (x) + "px").style('top', (y) + "px");
+    };
+
+    // Create Rectangles
     svg.selectAll("rect")
         .data(data)
         .join("rect")
-        .attr("class", "bar")
-        .attr("x", (d) => x_scale(d))
-        .attr("y", (d) => y_scale(dist.probabilities[d]))
-        .attr("width", x_scale.bandwidth())
-        .attr("height", (d) => height - (y_scale(dist.probabilities[d]) + margin.bottom));
+            .attr("class", "bar")
+            .attr("fill", "steelblue")
+            .attr("x", (d) => x_scale(d))
+            .attr("y", (d) => y_scale(dist.probabilities[d]))
+            .attr("width", x_scale.bandwidth())
+            .attr("height", (d) => height - (y_scale(dist.probabilities[d]) + margin.bottom))
+            .on("mousemove", mousemove)
+            .on("mouseover", mouseover)
+            .on("mouseout", mouseleave);
 
     // Create x-axis 
     svg.append("g")
@@ -57,7 +77,7 @@ export function graph_dist(dist) {
             .attr("text-anchor", "middle")
             .text("x"));
 
-
+    // Create y-axis
     svg.append("g")
     .attr("transform", `translate(${margin.left}, 0)`)
     .call(d3.axisLeft(y_scale).ticks(5))
